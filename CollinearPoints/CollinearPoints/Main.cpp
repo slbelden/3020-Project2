326009a0dfc22fa641c86c849361e04980181c17
Stephen L Belden <belden.stephen@gmail.com>
2015-09-28 15:58:32 -0600

    First.

---
+++ CollinearPoints/CollinearPoints/Main.cpp
@@ -0,0 +1,14 @@
+// Stephen Belden - SB
+// Meghan Haugaas - MH
+// Chris Ruiz - CR
+
+// 2015-Sept-28
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+	cout << "Hello World." << endl;
+}
